554066677cb75dfa01bcae81f2ad0b61235328f5
wz <wz7465@msn.com>
2026-06-13 22:38:11 +0800

    Initialize HTTP server in index.js

---
+++ api/index.js
@@ -0,0 +1,13 @@
+// index.js
+const http = require("http");
+
+const PORT = process.env.PORT; 
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello World");
+});
+
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
